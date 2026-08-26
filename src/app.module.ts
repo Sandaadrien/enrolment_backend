332e7db3aafbe82e772fdaa76562cf573811df2f
Sandaadrien <sandaniainaadrien@gmail.com>
2026-08-26 15:20:52 +0300

    test: trying swagger in render platform

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnrolmentModule } from './enrolment/enrolment.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [EnrolmentModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
